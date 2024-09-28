6ff09e72ba9380ac0cf3d4f4d2bee784a8ad7d06
VidhyaDevi <vidhyadevi.g@theaccessgroup.com>
2024-09-28 17:35:26 +0530

    learn ts

--- Test/TS/demo.ts
+++ Test/TS/demo.ts
@@ -1,2 +1,10 @@
-let rhithvik = "Ashok"
-console.log(typeof rhithvik)
+let h: boolean = false;
+let n1 = Math.random() * 100;
+
+if (n1 >= 1 && n1 < 50) {
+    h = true;
+    console.log('The number is between 1 & 50');
+} else {
+    h = false;
+    console.log('The number is between 50 & 100');
+}
