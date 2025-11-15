b59148edca0957930ddd9ea378fe10d501089058
Rhithvik <rhithvik.ashok@gmail.com>
2025-11-15 15:21:30 +0400

    completed_code

--- cricket_app_profesional_app/cricket_match_web_scraoing.py
+++ cricket_app_profesional_app/cricket_match_web_scraoing.py
@@ -12,15 +12,9 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
     
     # First 3 matches
-    score = soup.findAll('li', class_ = 'cb-view-all-ga cb-match-card cb-bg-white')
-    score = score[:3]
-    # Finding a tag so that 'shedule, table ' will not be vesible
-    for i in score:
-        score = i.find('a')
-        if score:
-            print(score.text, "\n")
-            pass
-        else:
-            print("Live score section not found.")
+    matches = soup.findAll('a', class_='h-full block w-[316px]')[:3]
+    print(matches [0])
+
+    
 else:
     print(f"Failed to fetch page. Status code: {response.status_code}")
