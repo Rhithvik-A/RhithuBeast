ea31fb5d90660518ec517124806277c0a918715a
Rhithvik <rhithvik.ashok@gmail.com>
2025-11-22 11:32:51 +0400

    new_deleted_and_cleared

--- Test/TS/demo2.ts
+++
@@ -1,8 +0,0 @@
-function addNumbers(num1: number, num2: number): number {
-    return num1 + num2;
-}
-
-let x = Math.random() * 100
-let y = Math.random() * 100
-let result = addNumbers(Math.floor(x), Math.floor(y));
-console.log("The sum is:", result);
